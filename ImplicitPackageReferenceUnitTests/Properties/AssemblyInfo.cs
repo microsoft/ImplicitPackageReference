99c1903e5ffa8736cc91ae9c82dc6fb1ace6c9d6
Microsoft Open Source <microsoftopensource@users.noreply.github.com>
2019-06-06 09:30:25 -0700

    Working beta for the ImplicitPackageReference. Unit tests created.

---
+++ ImplicitPackageReferenceUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImplicitPackageReferenceUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImplicitPackageReferenceUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26b369c2-aa3c-4dbe-9220-5bda6ef61068")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
